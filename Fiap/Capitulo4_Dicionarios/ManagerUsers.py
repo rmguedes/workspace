e3ab06ea10fb137f6cd92135ca72f5d8f0d1919c
Ricardo Malheiros Guedes <ricardo@dpc.srv.br>
2021-05-17 13:23:41 -0300

    Initial commit

---
+++ Fiap/Capitulo4_Dicionarios/ManagerUsers.py
@@ -0,0 +1,49 @@
+usuarios={}
+opcao=input("O que deseja realizar?
+" +
+            "<I> - Para Inserir um usuário
+"+
+            "<P> - Para Pesquisar um usuário
+"+
+            "<E> - Para Excluir um usuário
+"+
+            "<L> - Para Listar um usuário: ").upper()
+while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
+    if opcao=="I":
+        chave = input("Digite o login: ").upper()
+        usuarios[chave] = [input("Digite o nome: ").upper(),
+                           input("Digite a última data de acesso: "),
+                           input("Qual a última estação acessada: ").upper()]
+    opcao = input("O que deseja realizar?
+" +
+                  "<I> - Para Inserir um usuário
+" +
+                  "<P> - Para Pesquisar um usuário
+" +
+                  "<E> - Para Excluir um usuário
+" +
+                  "<L> - Para Listar um usuário: ").upper()
+
+from Capitulo4_Dicionarios.Funcoes import *
+usuarios={}
+opcao=perguntar()
+while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
+    if opcao=="I":
+        inserir(usuarios)
+    opcao = perguntar()
+
+from Capitulo4_Dicionarios.Funcoes import *
+usuarios={}
+
+opcao=perguntar()
+while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
+    if opcao=="I":
+        inserir(usuarios)
+    if opcao=="P":
+        pesquisar(usuarios,input("Qual login deseja pesquisar? "))
+    if opcao == "E":
+        excluir(usuarios,input("Qual login deseja excluir? "))
+    if opcao == "L":
+        listar(usuarios)
+    opcao = perguntar()
+    
